d6bfba44efa941c31d88a6e6ab1fac65172a8741
minhnhat1848936-maker <minhnhat1848936@gmail.com>
2026-04-07 22:00:15 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Ngày thi (sửa lại nếu muốn)
+const targetDate = new Date("June 1, 2026 00:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((distance / (1000 * 60)) % 60);
+  const seconds = Math.floor((distance / 1000) % 60);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+}, 1000);
