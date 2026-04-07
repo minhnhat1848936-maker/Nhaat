acb98e57555ef57a43490dd8d493706219a67e8e
minhnhat1848936-maker <minhnhat1848936@gmail.com>
2026-04-07 21:32:18 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const target = new Date("June 1, 2026 08:00:00").getTime();
-setInterval(() => {
-    const now = new Date().getTime();
-    const diff = target - now;
-    document.getElementById("days").innerText = Math.floor(diff / (1000 * 60 * 60 * 24));
-    document.getElementById("hours").innerText = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    document.getElementById("minutes").innerText = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-    document.getElementById("seconds").innerText = Math.floor((diff % (1000 * 60)) / 1000);
-}, 1000);
